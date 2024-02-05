a914dcfd8eb741d857209d043af177c7312c5354
Valentina Fuentes <fuentesvalentina74@gmail.com>
2024-02-05 13:57:08 -0300

    agrego bootstrap y cambio el titulo por hello world

--- 01ejercicioReact/src/App.jsx
+++ 01ejercicioReact/src/App.jsx
@@ -1,8 +1,12 @@
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css"
 
+function App() {
+//aqui agrego la logica del componente, dentro de la funcion pero antes del return
   return (
+    //aqui puedo escribir un poquito mas de logica
     <>
-      <h1>titulo de prueba</h1>
+    {/* aq */}
+      <h1 className="text-center">Hello world!</h1>
     </>
   )
 }
